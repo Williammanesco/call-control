7edd9e07bc3b293364e78a15046b41ad051924b0
William Manesco <manesco.william@gmail.com>
2016-06-30 18:48:35 -0300

    Alterado campos da colecao de grupos e usuarios grupo

--- modules/UsuariosGrupo/schema.js
+++ modules/UsuariosGrupo/schema.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
 const _Schema = {
   id_grupo: Schema.Types.ObjectId
-, id_usuario: Schema.Types.ObjectId
+, nome_usuario: String
+, telefone_usuario: String
 }
 
 module.exports = new Schema(_Schema);
