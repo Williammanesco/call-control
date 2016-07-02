6b92338d647747fd0d6a9cebeab77329b6f9afd1
William Manesco <manesco.william@gmail.com>
2016-07-02 17:57:32 -0300

    Schema chamadas

--- modules/ChamadasRealizadas/schema.js
+++ modules/ChamadasRealizadas/schema.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const _Schema = {
-  id_usuario: {
+  id_grupo: {
     type: Schema.Types.ObjectId
   , required: true
   , index: true
   }
-, tel_origem: { type: String }
-, duracao: { type: String }
-, finalizada: { type: String }
+, nome_usuario: String
+, telefone: String
+, duracao: String
+, finalizada: String
+, data: { type: Date, default: Date.now }
 }
 
 module.exports = new Schema(_Schema);
--- modules/ChamadasRecebidas/schema.js
+++ modules/ChamadasRecebidas/schema.js
@@ -1,14 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const _Schema = {
-  id_usuario: {
+  id_grupo: {
     type: Schema.Types.ObjectId
   , required: true
   , index: true
   }
-, tel_origem: { type: String }
-, duracao: { type: String }
-, finalizada: { type: String }
+, nome_usuario: String
+, telefone: String
+, duracao: String
+, finalizada: String
+, data: {type: Date, default: Date.now }
 }
+
+/*
+db.chamadasrealizadas.save({
+  id_grupo:ObjectId('57757fdb625836b00482c1bc'),
+  nome_usuario:'Alini linda',
+  telefone:'(12) 3131-2312',
+  duracao: '05:00',
+  finalizada: 'S'
+})
+*/
 
 module.exports = new Schema(_Schema);
